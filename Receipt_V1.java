d5cf0e117371e3e677d84351250d4508c31025c0
ENTITY <120266813+ENTITY040711@users.noreply.github.com>
2023-12-17 16:54:18 +0000

    Receipt System

--- Receipt_V1.java
+++ Receipt_V1.java
@@ -7,11 +7,10 @@ public class Receipt_V1 {
   public static void main(String[] args) {
     Scanner console = new Scanner(System.in);
 
-    //Variable for Date, Time and Tax Number
+    // Variable for Date, Time and Tax Number
     LocalDateTime now = LocalDateTime.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-      "yyyy-MM-dd HH:mm:ss"
-    );
+        "yyyy-MM-dd HH:mm:ss");
     String newDateTime = now.format(formatter);
     Random num = new Random();
     int taxNum = num.nextInt(9000);
@@ -21,7 +20,7 @@ public class Receipt_V1 {
     double cash = 0.0;
     String payPlatform, paymentMethod;
 
-    //Variable to keep items name, quantities, and prices
+    // Variable to keep items name, quantities, and prices
     String item;
     int qty = 0;
     double price = 0.0;
@@ -31,20 +30,19 @@ public class Receipt_V1 {
 
     System.out.println("WELCOME");
     System.out.println(
-      "Please enter your items details below. Type [OK] once finish"
-    );
+        "Please enter your items details below. Type [OK] once finish");
 
     while (true) {
       System.out.print("Item Name: ");
       item = console.nextLine();
       items.add(item);
 
-      //This section content function to stop the loop and proceed to checkout
+      // This section content function to stop the loop and proceed to checkout
       if (item.equalsIgnoreCase("OK") || item.equalsIgnoreCase("Okay")) {
         break;
       }
 
-      //While and Try-Catch are used to keep the loop until a valid input is receive
+      // While and Try-Catch are used to keep the loop until a valid input is receive
       System.out.print("Item Quantity: ");
       while (true) {
         try {
@@ -78,15 +76,13 @@ public class Receipt_V1 {
       prices.add(price);
     }
 
-    //This section is for payment method input
+    // This section is for payment method input
     while (true) {
       System.out.print("Please Specify Payment Method [CASH / CASHLESS]: ");
       paymentMethod = console.nextLine();
 
-      if (
-        paymentMethod.equalsIgnoreCase("Cash") ||
-        paymentMethod.equalsIgnoreCase("Cashs")
-      ) {
+      if (paymentMethod.equalsIgnoreCase("Cash") ||
+          paymentMethod.equalsIgnoreCase("Cashs")) {
         break;
       } else if (paymentMethod.equalsIgnoreCase("Cashless")) {
         break;
@@ -95,46 +91,38 @@ public class Receipt_V1 {
       }
     }
 
-    //This section is for receipt layout
+    // This section is for receipt layout
     System.out.println();
     System.out.println("  Cyberjaya Mall SDN BHD (00127)");
     System.out.println(" No 7, Desa Maju, 57100 Cyberjaya");
     System.out.println();
-    //taxNum is used for random tax number
+    // taxNum is used for random tax number
     System.out.println("        Tax Invoice " + taxNum);
     System.out.println("-----------------------------------");
 
-    /*why use .size? Because its a array list not normal array
-		 why have -1? Because to not print the OK for checkout loop*/
+    /*
+     * why use .size? Because its a array list not normal array
+     * why have -1? Because to not print the OK for checkout loop
+     */
     for (int index = 0; index < items.size() - 1; index++) {
       totalPrice = prices.get(index) * qtys.get(index);
 
-      //%-12s is for the padding % for printf, -12 for size of padding, s for string in printf
-      System.out.printf(
-        "%-12s *%-2d RM%-2.2f ~ RM%-2.2f%n",
-        items.get(index),
-        qtys.get(index),
-        prices.get(index),
-        totalPrice
-      );
+      // %-12s is for the padding % for printf, -12 for size of padding, s for string
+      // in printf
+      System.out.printf("%-12s *%-2d RM%-2.2f ~ RM%-2.2f%n", items.get(index), qtys.get(index), prices.get(index),
+          totalPrice);
       subTotal += totalPrice;
     }
 
     System.out.println("-----------------------------------");
     System.out.printf("%-27s RM%.2f%n", "Subtotal:", subTotal);
     System.out.printf("%-27s RM%.2f%n", "Tax 6%:", (tax = (subTotal * 0.06)));
-    System.out.printf(
-      "%-27s RM%.2f%n",
-      "Total:",
-      (finalTotal = (subTotal + tax))
-    );
+    System.out.printf("%-27s RM%.2f%n", "Total:", (finalTotal = (subTotal + tax)));
     System.out.println();
 
     while (true) {
-      if (
-        paymentMethod.equalsIgnoreCase("Cash") ||
-        paymentMethod.equalsIgnoreCase("Cashs")
-      ) {
+      if (paymentMethod.equalsIgnoreCase("Cash") ||
+          paymentMethod.equalsIgnoreCase("Cashs")) {
         System.out.printf("%-27s RM", "CASH:");
         cash = console.nextDouble();
 
@@ -154,13 +142,14 @@ public class Receipt_V1 {
     }
 
     System.out.println("-----------------------------------");
-    /*Why not using the padding method for printing the date and time? 
-		  Idk, I tried but does not work*/
+    /*
+     * Why not using the padding method for printing the date and time?
+     * Idk, I tried but does not work
+     */
     System.out.println("        " + newDateTime);
     System.out.println();
     System.out.println(
-      "            Thank You\n  For Shopping At Cyberjaya Mall\n        Please Come Again"
-    );
+        "            Thank You\n  For Shopping At Cyberjaya Mall\n        Please Come Again");
     console.close();
   }
 }
